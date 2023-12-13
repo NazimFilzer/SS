82c505f898b6b1b397a3f05b9ceb91227aeaf7c6
NzM <nazimfilzer@gmail.com>
2023-12-13 14:25:15 +0530

    passes

--- SS/passNa/pass2.c
+++ SS/passNa/pass2.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,37 +7,26 @@ void display();
 int main()
 {
     char a[10], ad[10], label[10], opcode[10], operand[10], symbol[10];
-    int start, diff, i, address, add, len, actual_len, finaddr, prevaddr, j = 0;
-    char mnemonic[15][15] = {"LDA", "STA", "LDCH", "STCH"};
-    char code[15][15] = {"33", "44", "53", "57"};
+    int start, diff, i, address, add, len, actual_len, finaddr, prevaddr, j = 0, length;
+    char mnemonic[10], code[10];
 
-    FILE *fp1, *fp2, *fp3, *fp4;
+    FILE *fp1, *fp2, *fp3, *fp4, *fp5, *fp6;
     fp1 = fopen("output.txt", "w");
     fp2 = fopen("symtab.txt", "r");
     fp3 = fopen("intermediate.txt", "r");
     fp4 = fopen("objcode.txt", "w");
-
+    fp5 = fopen("length.txt", "r");
+    fp6 = fopen("optab.txt", "r");
     fscanf(fp3, "%s\t%s\t%s", label, opcode, operand);
-
-    while (strcmp(opcode, "END") != 0)
-    {
-        prevaddr = address;
-        fscanf(fp3, "%d%s%s%s", &address, label, opcode, operand);
-    }
-    finaddr = address;
-
-    fclose(fp3);
-    fp3 = fopen("intermediate.txt", "r");
-
-    fscanf(fp3, "\t%s\t%s\t%s", label, opcode, operand);
+    fscanf(fp5, "%d", &length);
     if (strcmp(opcode, "START") == 0)
     {
+        start = atoi(operand);
         fprintf(fp1, "\t%s\t%s\t%s\n", label, opcode, operand);
-        fprintf(fp4, "H^%s^00%s^00%d\n", label, operand, finaddr);
+        fprintf(fp4, "H^%s^00%s^00%d\n", label, operand, length);
         fscanf(fp3, "%d%s%s%s", &address, label, opcode, operand);
-        start = address;
-        diff = prevaddr - start;
-        fprintf(fp4, "T^00%d^%d", address, diff);
+
+        fprintf(fp4, "T^00%d^%d", address, length);
     }
 
     while (strcmp(opcode, "END") != 0)
@@ -52,7 +39,9 @@ int main()
             fprintf(fp4, "^");
             for (i = 2; i < (actual_len + 2); i++)
             {
-                itoa(operand[i], ad, 16);
+                int val = operand[i];
+                // itoa(val, ad, 16);
+                sprintf(ad, "%x", val);
                 fprintf(fp1, "%s", ad);
                 fprintf(fp4, "%s", ad);
             }
@@ -61,31 +50,54 @@ int main()
 
         else if (strcmp(opcode, "WORD") == 0)
         {
-            len = strlen(operand);
-            itoa(atoi(operand), a, 10);
-            fprintf(fp1, "%d\t%s\t%s\t%s\t00000%s\n", address, label, opcode, operand, a);
-            fprintf(fp4, "^00000%s", a);
-        }
 
-        else if ((strcmp(opcode, "RESB") == 0) || (strcmp(opcode, "RESW") == 0))
+            int val = atoi(operand);
+            fprintf(fp1, "%d\t%s\t%s\t%s\t%06d\n", address, label, opcode, operand, val);
+            fprintf(fp4, "^%06d", val);
+        }
+        else if ((strcmp(opcode, "RESW") == 0))
         {
+
             fprintf(fp1, "%d\t%s\t%s\t%s\n", address, label, opcode, operand);
+            int val = atoi(operand);
+            while (val != 0)
+            {
+                fprintf(fp4, "^******");
+                val--;
+            }
         }
 
+        else if ((strcmp(opcode, "RESB") == 0))
+        {
+
+            fprintf(fp1, "%d\t%s\t%s\t%s\n", address, label, opcode, operand);
+            int val = atoi(operand);
+
+            fprintf(fp4, "^");
+            while (val != 0)
+            {
+                fprintf(fp4, "**");
+                val--;
+            }
+        }
+        // else if ((strcmp(opcode, "RESB") == 0) || (strcmp(opcode, "RESW") == 0)) {
+        //     fprintf(fp1, "%d\t%s\t%s\t%s\n", address, label, opcode, operand);
+        // }
         else
         {
-            while (strcmp(opcode, mnemonic[j]) != 0)
-                j++;
+            fscanf(fp6, "%s%s", mnemonic, code);
+            while (strcmp(opcode, mnemonic) != 0)
+                fscanf(fp6, "%s%s", mnemonic, code);
             if (strcmp(operand, "COPY") == 0)
-                fprintf(fp1, "%d\t%s\t%s\t%s\t%s0000\n", address, label, opcode, operand, code[j]);
+                fprintf(fp1, "%d\t%s\t%s\t%s\t%s0000\n", address, label, opcode, operand, code);
             else
             {
                 rewind(fp2);
                 fscanf(fp2, "%s%d", symbol, &add);
                 while (strcmp(operand, symbol) != 0)
                     fscanf(fp2, "%s%d", symbol, &add);
-                fprintf(fp1, "%d\t%s\t%s\t%s\t%s%d\n", address, label, opcode, operand, code[j], add);
-                fprintf(fp4, "^%s%d", code[j], add);
+                fprintf(fp1, "%d\t%s\t%s\t%s\t%s%d\n", address, label, opcode, operand, code, add);
+                fprintf(fp4, "^%s%d", code, add);
             }
         }
 
@@ -94,7 +106,8 @@ int main()
 
     fprintf(fp1, "%d\t%s\t%s\t%s\n", address, label, opcode, operand);
     fprintf(fp4, "\nE^00%d", start);
-
+    fclose(fp6);
+    fclose(fp5);
     fclose(fp4);
     fclose(fp3);
     fclose(fp2);
@@ -151,7 +164,6 @@ void display()
         ch = fgetc(fp4);
     }
     fclose(fp4);
-    printf("\n\n");
 }
 
 // char *itoa(int value, char *buffer, int base)
